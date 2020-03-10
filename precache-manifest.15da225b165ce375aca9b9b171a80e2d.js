3d9557887480407d02b51368f2bfb0df48e196b6
KHruthik <hketepalli@gmail.com>
2020-03-10 19:49:22 +0000

    Updates

---
+++ precache-manifest.15da225b165ce375aca9b9b171a80e2d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f77f4ad078573e19e1197835a6814d",
+    "url": "/BBC-article-ranker/index.html"
+  },
+  {
+    "revision": "1705e6979c92da9cd4e2",
+    "url": "/BBC-article-ranker/static/css/main.a00fab0b.chunk.css"
+  },
+  {
+    "revision": "5d18ea765b4b066211e8",
+    "url": "/BBC-article-ranker/static/js/2.c46ac76d.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/BBC-article-ranker/static/js/2.c46ac76d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1705e6979c92da9cd4e2",
+    "url": "/BBC-article-ranker/static/js/main.4b7808ab.chunk.js"
+  },
+  {
+    "revision": "22103d19a767a4202da9",
+    "url": "/BBC-article-ranker/static/js/runtime-main.cc9a0d80.js"
+  },
+  {
+    "revision": "e1f0bf095d199e48726ca90f4a966cd2",
+    "url": "/BBC-article-ranker/static/media/green_tick.e1f0bf09.png"
+  },
+  {
+    "revision": "0229c36488f67d3f208b2c0b60fc5247",
+    "url": "/BBC-article-ranker/static/media/logo.0229c364.png"
+  }
+]);
