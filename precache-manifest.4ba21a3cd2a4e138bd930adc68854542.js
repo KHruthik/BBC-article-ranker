0ca90603958cf3cc6329e2e39cec7c91056a4d10
KHruthik <hketepalli@gmail.com>
2020-03-12 12:36:51 +0000

    Updates

---
+++ precache-manifest.4ba21a3cd2a4e138bd930adc68854542.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a384158f7f9a13ae8081a7183ef20442",
+    "url": "/BBC-article-ranker/index.html"
+  },
+  {
+    "revision": "af2d157d99922fc957cd",
+    "url": "/BBC-article-ranker/static/css/main.a00fab0b.chunk.css"
+  },
+  {
+    "revision": "b78153d441d7e08a748b",
+    "url": "/BBC-article-ranker/static/js/2.3e7d664b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/BBC-article-ranker/static/js/2.3e7d664b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af2d157d99922fc957cd",
+    "url": "/BBC-article-ranker/static/js/main.ba85c1a5.chunk.js"
+  },
+  {
+    "revision": "22103d19a767a4202da9",
+    "url": "/BBC-article-ranker/static/js/runtime-main.cc9a0d80.js"
+  },
+  {
+    "revision": "e1f0bf095d199e48726ca90f4a966cd2",
+    "url": "/BBC-article-ranker/static/media/green_tick.e1f0bf09.png"
+  },
+  {
+    "revision": "0229c36488f67d3f208b2c0b60fc5247",
+    "url": "/BBC-article-ranker/static/media/logo.0229c364.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d6706fe81acdff46a9a9456ff09a94a.js"
+  "/BBC-article-ranker/precache-manifest.4ba21a3cd2a4e138bd930adc68854542.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BBC-article-ranker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
